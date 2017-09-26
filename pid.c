db9e595559893fe982ec69b7155c55f47f0e083c
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2017-09-26 21:06:31 +0200

    Add limit to the PID controller

--- pid.c
+++ pid.c
@@ -6,6 +6,7 @@
 
 struct PID {
 	double proportional, integral, derivative;
+	double upperLimit, lowerLimit;
 	double reset, oldError;
 };
 
@@ -34,7 +35,14 @@ double PID(double input, double setPoint, struct PID* restrict controller) {
 
 	printf("P: %3.3f I: %3.3f D: %3.3f ", p, i, d);
 
-	return p+i+d;
+	double pid = p + i + d;
+
+	pid = (pid < controller->lowerLimit) ? controller->lowerLimit : pid;
+	pid = (pid > controller->upperLimit) ? controller->upperLimit : pid;
+
+	printf("PID: %3.3f ", pid);
+
+	return pid;
 }
 
 double drag(double environment, double system, double scalar) {
@@ -57,6 +65,8 @@ void usage() {
 	printf("\t-r [integer]\tDrag scalar * 1000\n");
 	printf("\t-e [integer]\tEnvironment value * 1000\n");
 	printf("\t-f [integer]\tResponse factor* 1000\n");
+	printf("\t-u [integer]\tUpper PID limit * 1000\n");
+	printf("\t-l [integer]\tLower PID limit * 1000\n");
 	printf("\t-n [integer]\tNumber of samples\n");
 }
 
@@ -66,6 +76,8 @@ int main(int argc, char** argv) {
 		measurement = 2.0,
 		response = 1.0,
 		environment = 2.0,
+		upperLimit = 5.0,
+		lowerLimit = 5.0,
 		dragScalar = 0.2;
 
 	struct PID controller = { 
@@ -73,12 +85,15 @@ int main(int argc, char** argv) {
 		.integral = .5,
 		.derivative = .5,
 
+		.upperLimit = 5.0,
+		.lowerLimit = 5.0,
+
 		.reset = 0,
 		.oldError = 0,
 	};
 
 	int c = 0, iterations = 20;
-	while ((c = getopt (argc, argv, "p:i:d:n:s:m:r:e:f:")) != -1) {
+	while ((c = getopt (argc, argv, "p:i:d:n:s:m:r:e:f:u:l:")) != -1) {
 		switch (c) {
 		case '?':
 			usage();
@@ -106,6 +121,12 @@ int main(int argc, char** argv) {
 			break;
 		case 'e':
 			environment = (double)(atoi(optarg))/1000;
+			break;
+		case 'u':
+			controller.upperLimit = (double)(atoi(optarg))/1000;
+			break;
+		case 'l':
+			controller.lowerLimit = (double)(atoi(optarg))/1000;
 			break;
 		case 'f':
 			response = (double)(atoi(optarg))/1000;
